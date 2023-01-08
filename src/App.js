fc1574656a57b496bb5ef9be4067a578707ade79
RyanWatts1 <devshop202@yahoo.com>
2023-01-08 15:31:08 -0500

    dynamic expression Evaluation

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] =useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+
+
   return (
     <Fragment>
-      <Cart />
+      {cartIsShown && <Cart />}
       <Header />
       <main>
         <Meals />
