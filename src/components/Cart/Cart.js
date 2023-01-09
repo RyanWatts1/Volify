b6df20bedc1872a85898359aac683a0a170a4162
RyanWatts1 <devshop202@yahoo.com>
2023-01-09 13:08:41 -0500

    CartTester

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,7 +18,7 @@ const Cart = (props) => {
         <span>49.50</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
         <button className={classes.button}>Order</button>
--- src/components/Tech/TechItem/TechItem.js
+++ src/components/Tech/TechItem/TechItem.js
@@ -1,18 +1,18 @@
-import MealItemForm from "./TechItemForm";
+import TechItemForm from "./TechItemForm";
 import classes from "./TechItem.module.css";
 
 const TechItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
 
   return (
-    <li className={classes.meal}>
+    <li className={classes.tech}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <TechItemForm id={props.id} />
       </div>
     </li>
   );
