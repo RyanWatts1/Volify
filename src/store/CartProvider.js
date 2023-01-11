91aa57e1f52033f0e55f13fe03379d008b155920
RyanWatts1 <devshop202@yahoo.com>
2023-01-11 15:32:25 -0500

    Cart State and provider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,18 +1,37 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defualtCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  return defualtCartState;
+};
+
 const CartProvider = (props) => {
+  const [cartSate, dispatchCartAction] = useReducer(
+    cartReducer,
+    defualtCartState
+  );
+
   const addItemToCartHandler = (item) => {};
 
   const removeItemFromCartHandler = (id) => {};
 
   const cartContext = {
-    items:[],
-    totalAmount: 0,
+    items: cartSate.items,
+    totalAmount: defualtCartState.totalAmount,
     addItem: addItemToCartHandler,
-    removeItem: removeItemFromCartHandler
+    removeItem: removeItemFromCartHandler,
   };
 
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
